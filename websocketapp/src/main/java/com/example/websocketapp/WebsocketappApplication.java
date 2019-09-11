10910687ccbaefe6f2475a2765f8bdabc0760aee
sako01 <sargsyansak@gmail.com>
2019-09-11 09:58:00 +0400

    save message

---
+++ websocketapp/src/main/java/com/example/websocketapp/WebsocketappApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebsocketappApplication.class, args);
+  }
+
+}
